1cbade732ebed5014ca4f6d8be6e81132bf1968c
Ge Jin <jinwar@gmail.com>
2012-11-26 11:18:08 -0500

    commit reform_data.m

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ dispinversion
 =============
 
 Surface Wave Dispersion Inversion Code
+
+file: 
+reform_data.m: reform the data into the format that can be read in for later program.
---
+++ reform_data_png.m
@@ -0,0 +1,37 @@
+clear;
+
+noise = load('data/eikonal_avg.mat');
+
+xnode = noise.xnode;
+ynode = noise.ynode;
+
+selectperiods = 4:15
+pnum = 0;
+for ip = fliplr(selectperiods)
+	pnum = pnum+1;
+	tomo(pnum).phV = noise.avgtomo(ip).GV;
+	tomo(pnum).period = noise.periods(ip);
+	tomo(pnum).avgphV = nanmean(tomo(pnum).phV(:));
+	tomo(pnum).noise = true;
+end
+
+selectperiods = 0:4
+for ip = selectperiods
+	filename = sprintf('data/averageevent_%d.mat',ip);
+	eqdata = load(filename);
+	pnum = pnum+1;
+	tomo(pnum).phV = eqdata.newisoGV;
+	tomo(pnum).period = eqdata.periods(ip+1);
+	tomo(pnum).avgphV = nanmean(tomo(pnum).phV(:));
+	tomo(pnum).noise = false;
+end
+
+figure(22)
+clf
+hold on
+ind = find([tomo(:).noise]);
+plot([tomo(ind).period],[tomo(ind).avgphV],'-ro');
+ind = find(~[tomo(:).noise]);
+plot([tomo(ind).period],[tomo(ind).avgphV],'-bo');
+
+save('tomo.mat','tomo','xnode','ynode');
